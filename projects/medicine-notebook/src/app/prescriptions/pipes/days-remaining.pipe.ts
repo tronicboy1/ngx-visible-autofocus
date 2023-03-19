4e060b82a26297d4a17b12655fba647fbc785047
Austin John Mayer (井上 オースティン) <95405354+tronicboy1@users.noreply.github.com>
2023-03-20 06:50:58 +0900

    get rx list working good

--- projects/medicine-notebook/src/app/prescriptions/pipes/days-remaining.pipe.ts
+++ projects/medicine-notebook/src/app/prescriptions/pipes/days-remaining.pipe.ts
@@ -7,7 +7,7 @@ export class DaysRemainingPipe implements PipeTransform {
   transform(amountDispensed: number, dispensedAt: number): number {
     const today = new Date();
     const dispensedAtDate = new Date(dispensedAt);
-    const daysSinceDispensal = Math.ceil((today.getTime() - dispensedAtDate.getTime()) / (1000 * 60 * 60 * 24));
+    const daysSinceDispensal = Math.floor((today.getTime() - dispensedAtDate.getTime()) / (1000 * 60 * 60 * 24));
     return amountDispensed - daysSinceDispensal;
   }
 }
